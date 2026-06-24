b33c92a4c5e6cee0efa9cc1b7871e372301879c1
Nailton <nailtonrf@gmail.com>
2026-06-24 10:29:31 -0300

    feat: added IHaveCustomData interface and CustomData record

--- src/Funca.Abstractions/Metadata/CustomFieldMetadata.cs
+++ src/Funca.Abstractions/Metadata/CustomFieldMetadata.cs
@@ -4,10 +4,6 @@ namespace Funca.Abstractions.Metadata;
 
 public readonly record struct FieldId(string Value);
 
-public readonly record struct FieldValueId(string Value);
-
-public readonly record struct StateId(string Value);
-
 /// <summary>
 /// Represents the metadata for a custom field associated with a specific entity.
 /// This class provides detailed information about a custom field, including its
@@ -27,19 +23,4 @@ public sealed record CustomFieldMetadata : IRequireTenantPartition
     public string? DefaultValue { get; init; }
     public bool Required { get; init; }
     public bool Active { get; init; } = true;
-}
-
-/// <summary>
-/// Represents the value of a custom field for a specific entity.
-/// This class encapsulates the field value data, including its unique identifier,
-/// associated field identifier, and the actual value stored.
-/// </summary>
-public sealed record CustomFieldValue
-{
-    public required TenantId TenantId { get; init; }
-    public required string EntityName { get; init; }
-    public required StateId StateId { get; init; }
-    public required FieldValueId ValueId { get; init; }
-    public required FieldId FieldId { get; init; }
-    public required string Value { get; init; }
 }
---
+++ src/Funca.Abstractions/Metadata/IHaveCustomData.cs
@@ -0,0 +1,22 @@
+﻿namespace Funca.Abstractions.Metadata;
+
+/// <summary>
+/// Defines an interface for objects that have a collection of custom data.
+/// This interface is commonly used to represent entities or fields with
+/// additional metadata or information encapsulated in the form of custom data entries.
+/// </summary>
+public interface IHaveCustomData
+{
+    IEnumerable<CustomData> CustomData { get; }
+}
+
+/// <summary>
+/// Represents a piece of custom data associated with an entity or field.
+/// This class is typically used to store additional metadata or information
+/// that supplements the core properties of an entity.
+/// </summary>
+public sealed record CustomData
+{
+    public required FieldId Id { get; init; }
+    public required string Value { get; init; }
+}
